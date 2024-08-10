98db06a0ceb3436efdcf3a9238a0cfdb83e7ff6d
Araaish Paul <supersikhkid@gmail.com>
2024-08-10 15:21:01 -0400

    Add description page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import { invalidate, goto } from '$app/navigation';
 	import { initializeStores, Toast } from '@skeletonlabs/skeleton';
 	import Footer from '../lib/components/Footer.svelte';
-	
+
 	export let data;
 
 	let { supabase, session } = data;
@@ -23,8 +23,21 @@
 		return () => subscription.unsubscribe();
 	});
 
+	const handleHome = () => {
+		// if already on home page, refresh
+		if (location.pathname === '/') {
+			location.reload();
+		} else {
+			goto('/');
+		}
+	};
+
 	const handleLogin = () => {
 		goto('/auth/login');
+	};
+
+	const handleNani = () => {
+		goto('/nani');
 	};
 
 	initializeStores();
@@ -32,14 +45,19 @@
 
 <Toast />
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<img src="/jua-icon.png" alt="Jua Board icon" />
-				<h3 class="text-lg">Jua Board</h3>
+				<img on:click={() => handleHome()} src="/jua-icon.png" alt="Jua Board icon" />
+				<h3 on:click={() => handleHome()} class="text-lg">Jua Board</h3>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
+				<button class="btn btn-sm variant-filled-tertiary" on:click={() => handleNani()}
+					>なんですか</button
+				>
 				{#if session}
 					<form method="POST" action="auth?/logout">
 						<button class="btn btn-sm variant-filled-tertiary">Logout</button>
@@ -53,5 +71,5 @@
 		</AppBar>
 	</svelte:fragment>
 	<slot />
-	<svelte:fragment slot="pageFooter"><Footer/></svelte:fragment>
+	<svelte:fragment slot="pageFooter"><Footer /></svelte:fragment>
 </AppShell>
---
+++ src/routes/nani/+page.svelte
@@ -0,0 +1,28 @@
+<div class="container h-9/10 mx-auto flex-col justify-evenly items-center my-20">
+	<div class="card p-4 m-5">
+		<p>
+			Jua Board is a daily message board based on network effects and speculation. Any verified user
+			can submit one <i>bid</i> for the next day's message. A verified user can also submit one
+			<i>wager</i> for the next day's message. A wager is a prediction of what the next day's message
+			will be. Messages are limited to 40 characters.
+		</p>
+	</div>
+	<div class="card p-4 m-5">
+		<p>
+			At the end of the day at 00:00 UTC, a raffle takes place to determine the new daily message.
+			The new selected message is compared with all submitted wagers, and each user is assigned a
+			score based on the strength of their prediction. These scores are factored into the next day's
+			raffle.
+		</p>
+	</div>
+	<div class="card p-4 m-5">
+		<p>
+			All users' bids are assigned a score based on their wagers from the previous raffle. If a user
+			did not submit a wager on the previous day, their current bid is assigned an average score. The
+			winning bid is selected from a weighted random function. <b
+				>The better your prediction of tomorrow's message, the higher the chance of your message
+				being selected in the next raffle.</b
+			>
+		</p>
+	</div>
+</div>
